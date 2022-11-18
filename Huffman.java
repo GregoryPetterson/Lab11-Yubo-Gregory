fd9b97536eadffaa19426c419a8bfa5336cca81f
GregoryPetterson <89867803+GregoryPetterson@users.noreply.github.com>
2022-11-18 14:08:44 -0600

    Update Huffman.java

--- Huffman.java
+++ Huffman.java
@@ -2,15 +2,22 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Huffman {
     public static void main(String[] args){
-        Scanner inputfilename = new Scanner(System.in);
+        Scanner inputfilenamereader = new Scanner(System.in);
         System.out.println("Enter the file name of the alphabet and frequencies: ");
+        String inputfilename = inputfilenamereader.next();
+        inputfilenamereader.close();
      
 
-        Scanner outputfilename = new Scanner(System.in);
-        System.out.println("enter the output file name");
+        Scanner outputfilenamereader = new Scanner(System.in);
+        System.out.println("enter the output file name: ");
+        String outputfilename = outputfilenamereader.next();
+        outputfilenamereader.close();
         
+    }
 
+    private static String[] readData(String inputfilename){
 
+        
     }
     
 }
