30364c6e859871f7f808603889b4375081982176
GregoryPetterson <89867803+GregoryPetterson@users.noreply.github.com>
2022-11-18 13:56:11 -0600

    Add files via upload

---
+++ Huffman.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Huffman {
+    public static void main(String[] args){
+        Scanner inputfilename = new Scanner(System.in);
+        System.out.println("Enter the file name of the alphabet and frequencies: ");
+     
+
+        Scanner outputfilename = new Scanner(System.in);
+        System.out.println("enter the output file name");
+        
+
+
+    }
+    
+}
