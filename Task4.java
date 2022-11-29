3b7cda2d7a22aaf8754eff59ccabb7f67d9e623b
Gregory Peterson <pet03356@morris.umn.edu>
2022-11-29 15:32:58 -0600

    Made my own decode that takes the huffman encoding file, e or d, input file, and returns the input file encoded. Gonna make a decode next and do finish task 5 and task 6.

--- Task4.java
+++ Task4.java
@@ -1,91 +1,96 @@
-import java.util.ArrayDeque;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
 public class Task4 {
-    public static void main(String[] args) {
-        System.out.println("please input string:");
-        Scanner in = new Scanner(System.in);
-        String inLine = in.next();
-        in.close();
-        char[] chars = inLine.toCharArray();
-        Map<Character, Integer> characterMap = new HashMap<>();
+
     
-        for (int i = 0; i < chars.length; i++) {
-            int count = characterMap.containsKey(chars[i]) ? characterMap.get(chars[i]) : 0;
-            characterMap.put(chars[i], count + 1);
-        }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        if ((args.length != 3)) {
+			System.out.println("Please input the name of a file containing Huffman encodings, " +
+            "e (for encoding) or d (for decoding), and then the name of a text file.");
+			System.exit(0);
+		}
+
+        String huffmanencoding = args[0];
+		String  eORd = args[1];
+        String inputfilename = args[2];
+
+        // Encoding or decoding?
+        // Take the huffmanencoding file and turn it into hashmap so we can
+        // look up the code of a character or vice versa.
+        if(eORd.equals("e")){
+
+        Map<Character, String> characterMap = new HashMap<>();
+		Scanner in = new Scanner(new File(huffmanencoding));
+
+        File inputfile = new File(inputfilename);
+
+		while (in.hasNext()) {
+			String inLine = in.nextLine();
+			char key = inLine.charAt(0);
+			String code = inLine.substring(2);
+			characterMap.put(key, code);
+		}
+		in.close();
+
+            encode(characterMap, inputfile);
+        } else if( eORd.equals("d")){
+
+            Map<Character, String> codeMap = new HashMap<>();
+            Scanner in = new Scanner(new File(huffmanencoding));
     
-        PriorityQueue<HFNode> pQueue = new PriorityQueue<>();
-        Set<Character> key = characterMap.keySet();
-        for (char c : key) {
-            HFNode hfNode = new HFNode();
-            hfNode.setCharacter(c);
-            hfNode.setFrequence(characterMap.get(c));
-            pQueue.add(hfNode);
-        }
-        
-        // Building Huffman Tree
-        while (pQueue.size() > 1) {
-			HFNode newNode = new HFNode();
-			HFNode leftNode = pQueue.remove();
-			HFNode rightNode = pQueue.remove();
-			newNode.setFrequence(rightNode.getFrequence() + leftNode.getFrequence());
-			newNode.setLeft(leftNode);
-			newNode.setRight(rightNode);
-			newNode.setCode("");
-			newNode.setCharacter(newNode.getLeft().getCharacter());
-			pQueue.add(newNode);
-		}
- 
-		HFNode finalTree = pQueue.remove();
-		Queue<HFNode> queue = new ArrayDeque<>();
-		if (finalTree != null) {
-			queue.add(finalTree);
-			finalTree.getLeft().setCode("0");
-			finalTree.getRight().setCode("1");
-		}
-
-
-        Map<Character, String> dict = new HashMap<>();
- 
-		while (!queue.isEmpty()) {
-			HFNode node = queue.remove();
-			if (node.getLeft() != null)
-				node.getLeft().setCode(node.getCode() + "0");
-			if (node.getRight() != null)
-				node.getRight().setCode(node.getCode() + "1");
- 
-			if (node.getLeft() != null) {
-				queue.add(node.getLeft());
-			}
- 
-			if (node.getRight() != null) {
-				queue.add(node.getRight());
-			}
- 
-			if (node.getLeft() == null && node.getRight() == null) {
-				dict.put(node.character, node.code);
-			}
-		}
-
-        //Output
-        StringBuilder string = new StringBuilder();
-		for(Map.Entry<Character,Integer> e:characterMap.entrySet()){
-            System.out.println(e.getKey()+" "+e.getValue());
-        }
-		System.out.println("The result is:");
-		for(Map.Entry<Character,String> e:dict.entrySet()){
-            System.out.println(e.getKey()+" "+e.getValue());
-        }
-		for (int i = 0; i < chars.length; i++) {
-			string.append(dict.get(chars[i]));
-		}
-		System.out.println("The encode is:");
-		System.out.println(string);
+            File inputfile = new File(inputfilename);
+    
+            while (in.hasNext()) {
+                String inLine = in.nextLine();
+                char key = inLine.charAt(0);
+                String code = inLine.substring(2);
+                codeMap.put(key, code);
+            }
+            in.close();
+            
+            decode(codeMap, inputfile);
+        }    
     }
+
+    public static void encode(Map<Character, String> huffmanmap, File inputfile) throws FileNotFoundException{
+        Scanner in = new Scanner(inputfile);
+        String filestring = "";
+        
+
+        while (in.hasNext()) {
+			String inline = in.nextLine();
+            filestring = filestring + inline;
+		}
+        in.close();
+
+        filestring = filestring.toLowerCase();
+
+        char[] filechararray = filestring.toCharArray();
+        int chararraylength = filechararray.length;
+
+        PrintWriter outputfile = new PrintWriter(inputfile);
+
+        for(int i=0; i<chararraylength; i++){
+        String value = String.valueOf(huffmanmap.get(filechararray[i]));
+            
+        if (value != "null") {
+            outputfile.print(value);
+            
+            } else {
+                outputfile.print("");
+            }
+        }
+        outputfile.close();
+    }
+
+    public static void decode(Map<Character, String> huffmanmap, File inputfilename){
+        
+    }
+
+   
 }
